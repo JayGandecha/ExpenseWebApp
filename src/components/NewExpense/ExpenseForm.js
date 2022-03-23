016e25b502b45f4292e4f854db10b800cba7d705
JayGandecha <jgandecha5@gmail.com>
2022-03-23 15:40:31 +0530

    added two way data binidng

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
@@ -47,7 +47,10 @@ const ExpenseForm = () => {
             data: new Date(enteredDate)
         };
 
-        console.log(expenseData)
+        console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return ( 
@@ -55,15 +58,25 @@ const ExpenseForm = () => {
         <div className="new-expense__control">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type='text'onChange={titleChangeHandler} />
+                <input type='text'
+                value={enteredTitle} 
+                onChange={titleChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type='number' min="0.01" step="0.01" onChange={amountChangeHandler}/>
+                <input type='number' 
+                min="0.01" 
+                step="0.01" 
+                value={enteredAmount} 
+                onChange={amountChangeHandler}/>
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type='date' min="2022-04-01" max="2030-3-31" onChange={dateChangeHandler}/>
+                <input type='date' 
+                min="2022-04-01" 
+                max="2030-3-31" 
+                value={enteredDate} 
+                onChange={dateChangeHandler}/>
             </div>
         </div>
         <div className="new-expense__actions">
