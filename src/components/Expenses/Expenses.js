02fa8b86676cdd94feb6f67b3c13aecce4201b1b
JayGandecha <jgandecha5@gmail.com>
2022-05-18 22:57:44 +0530

    applied condition to shwo list of expensesw

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,18 @@ const filteredExpenses = props.items.filter(expense => {
   return expense.date.getFullYear().toString() === filteredYear;
 });
 
+let expenseContent = <p>No expense found.</p>;
+
+if(filteredExpenses.length > 0) {
+  expenseContent = filteredExpenses.map((expense) => (
+    <ExpenseItem
+      key={expense.id}
+      title={expense.title}
+      amount={expense.amount}
+      date={expense.date}
+    />
+    ))
+}
 
 return (
   <div>
@@ -24,15 +36,7 @@ return (
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expenseContent}
     </Card>
   </div>
   );
